00059a78d100d41f610383fbbab69109e08842db
Jianrui Hu <u0886750@utah.edum>
2019-09-11 22:08:45 -0600

    log in update

--- SportsMate/app/src/main/java/com/capstone/sportsmate/LoginActivity.java
+++ SportsMate/app/src/main/java/com/capstone/sportsmate/LoginActivity.java
@@ -2,24 +2,43 @@ package com.capstone.sportsmate;
 
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity
         implements View.OnClickListener {
+
+    private Button btLogin, btSignUp;
+    private EditText etEmail, etPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button buttonLogin = findViewById(R.id.button_Login);
-        Button buttonRegistered = findViewById(R.id.button_Registered);
+        btLogin = findViewById(R.id.button_Login);
+        btSignUp = findViewById(R.id.button_SignUp);
+        etEmail = findViewById(R.id.editText_Email);
+        etPassword = findViewById(R.id.editText_Password);
+        progressBar = findViewById(R.id.progressBar_Login);
 
-        buttonLogin.setOnClickListener(this);
-        buttonRegistered.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        btLogin.setOnClickListener(this);
+        btSignUp.setOnClickListener(this);
     }
 
     @Override
@@ -27,15 +46,54 @@ public class LoginActivity extends AppCompatActivity
         switch (view.getId()) {
 
             case R.id.button_Login: {
-
-
+                loginUserAccount();
+                break;
             }
 
-            case R.id.button_Registered: {
-                Intent messageIntent = new Intent(this, RegisterActivity.class);
+            case R.id.button_SignUp: {
+                Intent messageIntent = new Intent(LoginActivity.this, RegisterActivity.class);
                 this.startActivity(messageIntent);
+                break;
             }
 
         }
+    }
+
+    private void loginUserAccount() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email, password;
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            etPassword.getText().clear();
+                        }
+                    }
+                });
     }
 }
--- SportsMate/app/src/main/java/com/capstone/sportsmate/RegisterActivity.java
+++ SportsMate/app/src/main/java/com/capstone/sportsmate/RegisterActivity.java
@@ -1,13 +1,86 @@
 package com.capstone.sportsmate;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    private EditText etEmail, etPassword, etConfirmPassword;
+    private Button btRegister;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etEmail = findViewById(R.id.editText_EnterEmail);
+        etPassword = findViewById(R.id.editText_EnterPassword);
+        etConfirmPassword = findViewById(R.id.editText_ConfirmPassword);
+        btRegister = findViewById(R.id.button_Register);
+        progressBar = findViewById(R.id.progressBar_Register);
+
+        mAuth = FirebaseAuth.getInstance();
+        btRegister.setOnClickListener(this);
+    }
+
+    private void registerNewUser() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email, password, confirmPassword;
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+        confirmPassword = etConfirmPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(getApplicationContext(), "Password doesn't match", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        registerNewUser();
     }
 }
